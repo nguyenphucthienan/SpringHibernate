6d00eeaee1ca2e1a517a44169d543d85fe2aab9c
Nguyen Phuc Thien An <npta97@gmail.com>
2018-07-20 20:27:54 +0700

    Add spring-injecting-literal project

---
+++ spring-core/spring-injecting-literal/src/main/java/com/nguyenphucthienan/springcore/CricketCoach.java
@@ -0,0 +1,47 @@
+package com.nguyenphucthienan.springcore;
+
+public class CricketCoach implements Coach {
+    private FortuneService fortuneService;
+
+    private String emailAddress;
+    private String team;
+
+    // Create no-arg constructor
+    public CricketCoach() {
+        System.out.println("CricketCoach: inside no-arg constructor");
+    }
+
+    // Define setter method for dependency injection
+    public void setFortuneService(FortuneService fortuneService) {
+        System.out.println("CricketCoach: inside setter method - setFortuneService");
+        this.fortuneService = fortuneService;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        System.out.println("CricketCoach: inside setter method - setEmailAddress");
+        this.emailAddress = emailAddress;
+    }
+
+    public String getTeam() {
+        return team;
+    }
+
+    public void setTeam(String team) {
+        System.out.println("CricketCoach: inside setter method - setTeam");
+        this.team = team;
+    }
+
+    @Override
+    public String getDailyWorkout() {
+        return "Practice fast bowling";
+    }
+
+    @Override
+    public String getDailyFortune() {
+        return fortuneService.getFortune();
+    }
+}
---
+++ spring-core/spring-injecting-literal/src/main/java/com/nguyenphucthienan/springcore/HappyFortuneService.java
@@ -0,0 +1,7 @@
+package com.nguyenphucthienan.springcore;
+
+public class HappyFortuneService implements FortuneService {
+    public String getFortune() {
+        return "Today is your lucky day!";
+    }
+}
