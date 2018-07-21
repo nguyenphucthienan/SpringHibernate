9b4efe2e13dbc112a136ed3b76fb26edcae903f5
Nguyen Phuc Thien An <npta97@gmail.com>
2018-07-21 13:03:18 +0700

    Add spring-annotation-qualifiers project

---
+++ spring-core/spring-annotation-qualifiers/src/main/java/com/nguyenphucthienan/springcore/BaseballCoach.java
@@ -0,0 +1,22 @@
+package com.nguyenphucthienan.springcore;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BaseballCoach implements Coach {
+    @Autowired
+    @Qualifier("randomFortuneService")
+    private FortuneService fortuneService;
+
+    @Override
+    public String getDailyWorkout() {
+        return "Spend 30 minutes on batting practice";
+    }
+
+    @Override
+    public String getDailyFortune() {
+        return fortuneService.getFortune();
+    }
+}
