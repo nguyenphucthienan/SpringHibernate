3bce8914aabc152115dd83f2a835a3779e838380
Nguyen Phuc Thien An <npta97@gmail.com>
2018-07-22 15:44:34 +0700

    Add spring-mvc-create-home-controller-and-view project

---
+++ spring-mvc/spring-mvc-create-home-controller-and-view/src/main/java/com/nguyenphucthienan/springmvc/controller/HomeController.java
@@ -0,0 +1,12 @@
+package com.nguyenphucthienan.springmvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    @RequestMapping("/")
+    public String getIndexPage() {
+        return "index";
+    }
+}
