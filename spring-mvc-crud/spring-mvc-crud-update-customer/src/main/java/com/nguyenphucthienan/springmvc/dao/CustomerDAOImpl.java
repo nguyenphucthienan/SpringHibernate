5d3d3754ad03f948d51d374cba3f5eb75f5adeeb
Nguyen Phuc Thien An <npta97@gmail.com>
2018-07-27 19:09:15 +0700

    Add spring-mvc-crud-update-customer project

---
+++ spring-mvc-crud/spring-mvc-crud-update-customer/src/main/java/com/nguyenphucthienan/springmvc/dao/CustomerDAOImpl.java
@@ -0,0 +1,37 @@
+package com.nguyenphucthienan.springmvc.dao;
+
+import com.nguyenphucthienan.springmvc.entity.Customer;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class CustomerDAOImpl implements CustomerDAO {
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    @Override
+    public List<Customer> getCustomers() {
+        Session session = sessionFactory.getCurrentSession();
+        Query<Customer> query = session.createQuery("from Customer order by firstName", Customer.class);
+        List<Customer> customers = query.getResultList();
+        return customers;
+    }
+
+    @Override
+    public Customer getCustomer(int id) {
+        Session session = sessionFactory.getCurrentSession();
+        Customer customer = session.get(Customer.class, id);
+        return customer;
+    }
+
+    @Override
+    public void saveCustomer(Customer customer) {
+        Session session = sessionFactory.getCurrentSession();
+        session.saveOrUpdate(customer);
+    }
+}
---
+++ spring-mvc-crud/spring-mvc-crud-update-customer/src/main/java/com/nguyenphucthienan/springmvc/service/CustomerService.java
@@ -0,0 +1,11 @@
+package com.nguyenphucthienan.springmvc.service;
+
+import com.nguyenphucthienan.springmvc.entity.Customer;
+
+import java.util.List;
+
+public interface CustomerService {
+    List<Customer> getCustomers();
+    Customer getCustomer(int id);
+    void saveCustomer(Customer customer);
+}
