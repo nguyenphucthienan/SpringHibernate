60d202faabcf4320b588e003cc997228926b41c5
Nguyen Phuc Thien An <npta97@gmail.com>
2018-07-27 17:01:56 +0700

    Add spring-mvc-crud-refactor-code project

---
+++ spring-mvc-crud/spring-mvc-crud-refactor-code/src/main/java/com/nguyenphucthienan/springmvc/controller/CustomerController.java
@@ -0,0 +1,21 @@
+package com.nguyenphucthienan.springmvc.controller;
+
+import com.nguyenphucthienan.springmvc.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/customer")
+public class CustomerController {
+    @Autowired
+    private CustomerService customerService;
+
+    @GetMapping("/list")
+    public String listCustomers(Model model) {
+        model.addAttribute("customers", customerService.getCustomers());
+        return "list-customers";
+    }
+}
---
+++ spring-mvc-crud/spring-mvc-crud-refactor-code/src/main/java/com/nguyenphucthienan/springmvc/service/CustomerService.java
@@ -0,0 +1,9 @@
+package com.nguyenphucthienan.springmvc.service;
+
+import com.nguyenphucthienan.springmvc.entity.Customer;
+
+import java.util.List;
+
+public interface CustomerService {
+    List<Customer> getCustomers();
+}
