a827700d64c6c026ce402c4d2e08a84647378dda
Nguyen Phuc Thien An <npta97@gmail.com>
2018-07-29 13:09:56 +0700

    Add spring-security-jdbc-bcrypt project

---
+++ spring-security/spring-security-jdbc-bcrypt/src/main/java/com/nguyenphucthienan/springsecurity/config/AppConfig.java
@@ -0,0 +1,64 @@
+package com.nguyenphucthienan.springsecurity.config;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import javax.sql.DataSource;
+import java.beans.PropertyVetoException;
+import java.util.logging.Logger;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.nguyenphucthienan.springsecurity")
+@PropertySource("classpath:persistence-mysql.properties")
+public class AppConfig {
+    private Logger logger = Logger.getLogger(AppConfig.class.getName());
+
+    @Autowired
+    private Environment env;
+
+    @Bean
+    public ViewResolver viewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/WEB-INF/view/");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+    }
+
+    @Bean
+    public DataSource securityDataSource() {
+        ComboPooledDataSource securityDataSource = new ComboPooledDataSource();
+
+        try {
+            securityDataSource.setDriverClass(env.getProperty("jdbc.driver"));
+        } catch (PropertyVetoException e) {
+            throw new RuntimeException(e);
+        }
+
+        logger.info("JDBC URL: " + env.getProperty("jdbc.url"));
+        logger.info("JDBC User" + env.getProperty("jdbc.user"));
+
+        securityDataSource.setJdbcUrl(env.getProperty("jdbc.url"));
+        securityDataSource.setUser(env.getProperty("jdbc.user"));
+        securityDataSource.setPassword(env.getProperty("jdbc.password"));
+
+        securityDataSource.setInitialPoolSize(getIntProperty("connection.pool.initialPoolSize"));
+        securityDataSource.setMinPoolSize(getIntProperty("connection.pool.minPoolSize"));
+        securityDataSource.setMaxPoolSize(getIntProperty("connection.pool.maxPoolSize"));
+        securityDataSource.setMaxIdleTime(getIntProperty("connection.pool.maxIdleTime"));
+
+        return securityDataSource;
+    }
+
+    private int getIntProperty(String propertyName) {
+        return Integer.parseInt(env.getProperty(propertyName));
+    }
+}
