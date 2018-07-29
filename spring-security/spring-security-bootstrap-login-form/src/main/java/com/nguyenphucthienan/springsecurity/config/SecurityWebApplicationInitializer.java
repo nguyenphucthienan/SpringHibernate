482c7716b10f0c59ebb7fca33a3c09b1a76a5c25
Nguyen Phuc Thien An <npta97@gmail.com>
2018-07-29 10:12:08 +0700

    Add spring-security-bootstrap-login-form project

---
+++ spring-security/spring-security-bootstrap-login-form/src/main/java/com/nguyenphucthienan/springsecurity/config/SecurityWebApplicationInitializer.java
@@ -0,0 +1,6 @@
+package com.nguyenphucthienan.springsecurity.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+}
